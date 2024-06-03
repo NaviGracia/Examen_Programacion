ef51b5f160037ffe8769eee68c2e6a1a1941f6e3
NaviGracia <ivagartor2@alu.edu.gva.es>
2024-06-03 12:41:22 +0200

    a

--- Examen/src/BD.java
+++ Examen/src/BD.java
@@ -12,57 +12,82 @@ import java.sql.Statement;
 
 public class BD extends Entrada_Salida{
     public static Connection conexion = null;
-    public static Statement st = null;
-    public static PreparedStatement sentencia = null;
-    public static ResultSet rs = null;
 
-    public BD(){}
+    public Connection cargarBD(String nombreBD, String usuario, String contrasenya) throws Exception{
+        try {
+            Class.forName("org.postgresql.Driver");   
+        } catch (Exception e) {
+            System.out.println("Error al registrar el driver");
+        }
 
-    public String cargarBD(String nombreBD, String usuario, String contrasenya) throws Exception{
-        Class.forName("org.postgresql.Driver");
-        conexion = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + nombreBD, usuario, contrasenya);
-        st = conexion.createStatement();
-        return "BD cargada.";
-    }    
+        try {
+            conexion = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + nombreBD, usuario, contrasenya);  
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conexion;
+    }   
+    
+    public static void crearTabla(){
+        String vacio = "CREATE TABLE prueba (column1 int);";
+        PreparedStatement st;
+        try {
+            st = conexion.prepareStatement(vacio);
+            st.execute();
+        } catch (Exception e) {
+            System.out.println("Fallo en la creación de la tabla: " + e);
+        }
+    }
+
+    public static void cerrarBD(){
+        try {
+            conexion.close();
+            conexion = null;
+        } catch (Exception e) {
+            System.out.println("Fallo cerrando la conexión a la BD");
+        }
+    }
 
     public static void insertarBD() throws Exception{
         String sql = "INSERT INTO  VALUES(?, ?, ?, ?)";
-        sentencia = conexion.prepareStatement(sql);
-        sentencia.setInt(1, devolverInt());;
-        sentencia.setString(2, devolverString());
-        sentencia.setString(3, devolverString());
-        sentencia.setDouble(4, devolverDouble());
-        sentencia.executeUpdate();
+        PreparedStatement st = conexion.prepareStatement(sql);
+        st.setInt(1, devolverInt());;
+        st.setString(2, devolverString());
+        st.setString(3, devolverString());
+        st.setDouble(4, devolverDouble());
+        st.executeUpdate();
     }
 
     public static void actualizarBD() throws Exception{
         String sql = "UPDATE SET ? = ? WHERE  = ?";
-        sentencia = conexion.prepareStatement(sql);
-        sentencia.setString(1, devolverString());
-        sentencia.executeUpdate();
+        PreparedStatement st = conexion.prepareStatement(sql);
+        st.setString(1, devolverString());
+        st.executeUpdate();
     }
 
     public static void eliminarBD() throws Exception{
         String sql = "DELETE FROM carta WHERE n_carta = ?";
-        sentencia = conexion.prepareStatement(sql);
-        sentencia.setInt(1, devolverInt());
-        sentencia.executeQuery();
+        PreparedStatement st = conexion.prepareStatement(sql);
+        st.setInt(1, devolverInt());
+        st.executeQuery();
     }
 
     public static void registrarNuevaCartaHashMap(int nCarta) throws Exception{
-        ResultSet rs = st.executeQuery("SELECT ca.n_carta, ca.nombre, ca.categoria, ca.precio, ca.stock, l.fuerza, l.resistencia, l.velocidad, l.carisma FROM carta ca LEFT JOIN luchador l ON ca.n_carta = l.n_carta WHERE ca.n_carta = " + nCarta);
+        PreparedStatement st = conexion.prepareStatement("SELECT ca.n_carta, ca.nombre, ca.categoria, ca.precio, ca.stock, l.fuerza, l.resistencia, l.velocidad, l.carisma FROM carta ca LEFT JOIN luchador l ON ca.n_carta = l.n_carta WHERE ca.n_carta = " + nCarta);
+        ResultSet rs = st.executeQuery();
         while (rs.next()) {
             
         }
     }
 
-    public static void idAutogenerado() throws Exception{
-        st = conexion.createStatement();
 
-        st.executeUpdate("CREATE TABLE EMP_PHONE (EMPNO CHAR(6), PHONENO CHAR(4), " + "IDENTCOL INTEGER GENERATED ALWAYS AS IDENTITY)"); 
-        sentencia =conexion.prepareStatement("INSERT INTO EMP_PHONE (EMPNO, PHONENO) " + "VALUES ('000010', '5555')", Statement.RETURN_GENERATED_KEYS);
+    public static void idAutogenerado() throws Exception{
+        Statement stm = conexion.createStatement();
+
+        stm.executeUpdate("CREATE TABLE EMP_PHONE (EMPNO CHAR(6), PHONENO CHAR(4), " + "IDENTCOL INTEGER GENERATED ALWAYS AS IDENTITY)"); 
+        stm = conexion.prepareStatement("INSERT INTO EMP_PHONE (EMPNO, PHONENO) " + "VALUES ('000010', '5555')", Statement.RETURN_GENERATED_KEYS);
                                       
-        rs = st.getGeneratedKeys(); 
+        ResultSet rs = stm.getGeneratedKeys(); 
                                       
         while (rs.next()) {
             java.math.BigDecimal idColVar = rs.getBigDecimal(1);     
@@ -70,6 +95,6 @@ public class BD extends Entrada_Salida{
             System.out.println("automatically generated key value = " + idColVar);
         }
         rs.close();
-        st.close();
+        stm.close();
     }
 }
