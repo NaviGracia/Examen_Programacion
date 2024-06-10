2d37e1f996dd66fa0555e6521d330e9c18ff6349
NaviGracia <ivagartor2@alu.edu.gva.es>
2024-06-10 11:56:48 +0200

    a

--- Examen Parte 2/src/FicheroBytes.java
+++ Examen Parte 2/src/FicheroBytes.java
@@ -8,14 +8,19 @@ import java.io.*;
 public class FicheroBytes extends Entrada_Salida{
         public void escribirBytes(){
             FileOutputStream fout = new FileOutputStream(/*Ruta fichero a escribir */);
+            DataOutputStream dos = new DataOutputStream(fout);
             System.out.println("Byte a escribir");
             byte b = devolverByte();
-            fout.write(b);
+            dos.write(b);
+            dos.close();
+            fout.close();
         }
 
         public void leerBytes(){
             FileInputStream fis = new FileInputStream(/*Ruta del archivo */)
-            
+            DataInputStream dis = new DataInputStream(fis);
+            System.out.println(dis.readUTF());
+            System.out.println(dis.readInt());
         }
 }
 
