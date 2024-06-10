f79648a4fe4f1727fb958430d7c918b45ddcfef8
NaviGracia <ivagartor2@alu.edu.gva.es>
2024-06-10 12:13:22 +0200

    a

--- Examen Parte 2/src/FicheroDatos.java
+++ Examen Parte 2/src/FicheroDatos.java
@@ -1,7 +1,25 @@
+import java.io.*;
+
 /**
  * FicheroDatos
  */
-public class FicheroDatos {
+public class FicheroDatos extends Entrada_Salida{
 
-    
+    public void escribirDatos(){
+            FileWriter fw = new FileWriter(/*Ruta fichero a escribir */);
+            BufferedWriter bw = new BufferedWriter(fw);
+            System.out.println("Texto a escribir");
+            String texto = devolverString();
+            bw.write(texto);
+            bw.close();
+            fw.close();
+        }
+
+        public void leerDatos(){
+            FileReader fr = new FileReader(/*Ruta del archivo */);
+            BufferedReader br = new BufferedReader(fr);
+            System.out.println(br.readLine());//Lee la primera linea
+            br.close();
+            fr.close();
+        }
 }
